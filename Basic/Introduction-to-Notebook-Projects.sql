b798e1b0120e91995b87a58e133b01e2ce7211f5
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-03-04 00:04:08 +0100

    Add SQL solution

---
+++ Basic/Introduction-to-Notebook-Projects.sql
@@ -0,0 +1,23 @@
+-- Project: Introduction to Notebook Projects
+-- Dataset: Bicycle Sales
+-- Task:
+-- Retrieve the customer_id for a specific order.
+
+
+-- Instructions:
+-- 1. Select the customer_id
+-- 2. From the sales.orders table
+-- 3. Filter the results using order_id
+-- 4. Update the order_id value as instructed before submission
+
+--SELECT
+--    customer_id
+--FROM sales.orders
+--WHERE order_id = 9;-- replace with 10 and submit to complete the project.
+
+-- MY SOLUTION:
+
+SELECT
+    customer_id
+FROM sales.orders
+WHERE order_id = 10;
